867a0d120b118eea9689ff835e48813d0e814d74
Kajal Sharma <kajalssharma.25@gmail.com>
2025-09-30 11:50:28 -0400

    Created Person and Student class

---
+++ Competition2.cs
@@ -0,0 +1,33 @@
+namespace ISM6225_Competition2
+{
+    public class Person
+    {
+        // Properties common to all persons
+        public string Name;
+        public string ID;
+        public string Email;
+
+        // Constructor
+        public Person(string name, string id, string email)
+        {
+            Name = name;
+            ID = id;
+            Email = email;
+        }
+    }
+
+    public class Student : Person
+    {
+        // Additional properties specific to students
+        public string Major;
+        public int GraduationYear;
+
+        // Constructor
+        public Student(string name, string id, string email, string major, int year)
+            : base(name, id, email)
+        {
+            Major = major;
+            GraduationYear = year;
+        }
+    }
+}
